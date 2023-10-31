87ab160c423b59347c5da9c97733e0762433fb67
Damian Esene <damianesene@Damians-MacBook-Air.local>
2023-10-31 14:48:33 -0400

    add clear chat button

--- streamlit/app1.py
+++ streamlit/app1.py
@@ -7,9 +7,15 @@ import openai
 
 #app = Flask(__name__)
 class App():
+    st.set_page_config(page_title="Cloud chatbot")
+    def clear_chat():
+        st.session_state.chat_history = []
+
     def run(self):
-        st.set_page_config(page_title="Cloud chatbot")
+        
+        
         with st.sidebar:
+            #st.button("clear chat", on_click=clear_chat())
             openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
             "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
             "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
@@ -19,30 +25,105 @@ class App():
         openai.api_key = openai_api_key
         if "messages" not in st.session_state:
             st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-        #bot_msg = st.chat_message("assistant")
-        #bot_msg.write("How May I help you today?")
         for msg in st.session_state.messages:
             st.chat_message(msg["role"]).write(msg["content"])
+        if 'chat_history' not in st.session_state:
+            st.session_state["chat_history"] = []
+        for user_msg in st.session_state.chat_history:
+            
+            st.chat_message(user_msg["role"]).write(user_msg["content"])
+
+        
+        
+        #bot_msg = st.chat_message("assistant")
+        #bot_msg.write("How May I help you today?")
+        
+        
         user_input = st.chat_input(
                             "ask a question", key="user_input")
-        if 'chat_history' not in st.session_state:
-            st.session_state.chat_history = []
+        
+        
+     
+        #st.session_state.clicked = False
+        
+        
+            #st.button('Clear Chat', on_click=st.session_state.chat_history.clear())
+            #user_chat = True
+            #while user_chat:
+        #reset_butt = st.button('Clear',on_click=st.session_state.chat_history.clear())
         if st.session_state.user_input:
-            st.session_state.chat_history.append(("response", st.session_state.user_input))
+            
+            #st.session_state.messages.append({"role":"user","content":user_input})
+            
+            st.session_state.chat_history.append({"role":"user","content":user_input})
             #st.session_state.user_input = ""
             st.chat_message("user").write(user_input)
-            bot_reply = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages )
+            
+            #bot_reply = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages )
+            bot_reply = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.chat_history )
             #messages=st.session_state.messages))
             msg = bot_reply.choices[0].message
-            st.session_state.messages.append(msg)
+            #st.session_state.messages.append(msg)
+            st.session_state.chat_history.append(msg)
             st.chat_message("assistant").write(msg.content)
+    with st.sidebar:
+
+       if st.button('clear chat'):
+            clear_chat()
+        
+        
+        #if reset_butt:
+            #st.button('Clear chat',on_click=st.session_state.chat_history.clear())
+            #st.session_state.chat_history=[]
+    #def clear_chat(self):
+   
+        #if st.button('Clear Chat'):
+        #st.button("Clear Chat", on_click=st.session_state.chat_history.clear())
+                #reset_butt = st.button('Clear chat',on_click=st.session_state.chat_history.clear())
+
+
+        
+        #if reset_butt:
+                #st.button('Clear chat', on_click=st.session_state.messages.clear())
+            
+            #if reset_butt:
+             #   st.session_state.chat_history.clear()
+            
+
+
+        #clear_hist = st.session_state.chat_history = []
+        
+        
+    
+        
+            #def clear_chat():
+        #st.session_state.messages = st.session_state.messages[1:]
+                #st.button('Clear Chat', on_click=st.session_state.chat_history.clear())
+            #clear_chat()
+        
+        #clear_chat = del st.session_state[messages]
+
+        # reset_chat = st.button("clear chat")
+        # if reset_chat:
+        #     st.session_state.messages = None
+
+       # def clear_chat():
+            #del st.session_state["chat_history"]
+
+        #    st.session_state.messages =  None
+             
+        #st.button('Clear Chat', on_click=clear_chat())
+        
         #if user_input:
 
         #st.session_state.my_text.append()
         #if "messages" not in st.session_state:
          #   st.session_state["messages"] = [{"role": "assistant", "content": "how can i help you?"}]
 
+  
 
 if __name__ =="__main__":
+ 
     app = App()
     app.run()
+    
--- streamlit/test_1.py
+++ streamlit/test_1.py
@@ -1,5 +1,49 @@
-import json
+import streamlit as st
+import openai
 
-response_1 = '{"detail": "Method not allowed"}'
-request_obj = json.loads(response_1)
-print(request_obj["detail"])
+# Set your OpenAI API key here
+openai.api_key = "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"
+
+class ChatBotApp:
+    def __init__(self):
+        self.chat_history = []
+
+    def run(self):
+        st.title("OpenAI Chat Bot")
+
+        # Display chat history
+        st.header("Chat History")
+        for message in self.chat_history:
+            st.write(message)
+
+        # Text input for user to type a message
+        user_message = st.text_input("Your Message")
+
+        # Button to send a message to the chatbot
+        if st.button("Send"):
+            if user_message:
+                self.add_to_chat_history(f"You: {user_message}")
+                bot_response = self.get_bot_response(user_message)
+                self.add_to_chat_history(f"Bot: {bot_response}")
+
+        # Button to clear chat history
+        if st.button("Clear Chat History"):
+            self.clear_chat_history()
+
+    def add_to_chat_history(self, message):
+        self.chat_history.append(message)
+
+    def clear_chat_history(self):
+        self.chat_history = []
+
+    def get_bot_response(self, user_message):
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"You: {user_message}\nBot:",
+            max_tokens=50
+        )
+        return response.choices[0].text.strip()
+
+if __name__ == "__main__":
+    app = ChatBotApp()
+    app.run()
